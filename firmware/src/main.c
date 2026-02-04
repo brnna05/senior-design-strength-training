280a4f082c942c6e92f0dd720f4d552f5ee5409e
Brianna Aguilar <155051255+brnna05@users.noreply.github.com>
2026-02-04 14:45:44 -0600

    it builds ig

--- firmware/src/main.c
+++ firmware/src/main.c
@@ -1,6 +1,17 @@
 #include <zephyr/kernel.h>
+#include <zephyr/bluetooth/bluetooth.h>
 
 int main(void)
 {
+    int err = bt_enable(NULL);
+    if (err) {
+        printk("BLE init failed (%d)\n", err);
         return 0;
+    }
+
+    printk("Bluetooth ready\n");
+
+    while (1) {
+        k_sleep(K_SECONDS(1));
+    }
 }
